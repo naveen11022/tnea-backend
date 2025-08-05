ee29d8d028f0b64cdb72ba6e546f008e0c62c4fb
Naveen Prasath <naviprasaad50@gmail.com>
2025-08-05 23:24:15 +0530

    db credentials updated

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("mysql+pymysql://root:mysql123@localhost:3306/tnea")
+# engine = create_engine("mysql+pymysql://root:mysql123@localhost:3306/tnea")
+engine = create_engine("mysql+pymysql://root:M3ER8MfzddLDjPdf@161.97.142.217:13306/tnea")
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
