5f2598a246c2324b77ba3b47c26bc6a0badd9aef
Naveen Prasath <naviprasaad50@gmail.com>
2025-08-06 00:15:25 +0530

    updated session

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:mysql123@localhost:3306/tnea"
+engine = create_engine("mysql+pymysql://tnea_admin:M3ER8MfzddLDjPdf@161.97.142.217:13306/tnea")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
