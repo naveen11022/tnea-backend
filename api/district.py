4940003705524dc8d5d4e073e2ca92064df472a8
Naveen Prasath <naviprasaad50@gmail.com>
2025-12-15 12:06:41 +0530

    modify caching logic in district

--- api/district.py
+++ api/district.py
@@ -11,10 +11,6 @@ router = APIRouter()
 @router.post('/districts')
 def get_districts(district: Regions, db: Session = Depends(get_db)):
     try:
-        cache_key = "districts"
-        cache_data = redis_client.get(cache_key)
-        if cache_data:
-            return json.loads(cache_data)
         districts_query = (
             db.query(Colleges.location)
             .filter(Colleges.region.in_(district.District))
@@ -23,12 +19,11 @@ def get_districts(district: Regions, db: Session = Depends(get_db)):
         )
         results = [loc[0] for loc in districts_query]
         results.sort()
-        redis_client.setex(cache_key,3600,json.dumps(results))
         return results
 
     except Exception as e:
         print("Error fetching districts:", e)
-
+        return []
 
 @router.get("/college_type")
 def get_college(db:Session=Depends(get_db)):
