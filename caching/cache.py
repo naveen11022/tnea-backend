01fbecdf7e3c11c3671d0ca88f97ca7237fbc4d9
Naveen Prasath S <naviprasaad50@gmail.com>
2026-02-28 07:37:30 +0530

    Fix Redis client variable name for connection

--- caching/cache.py
+++ caching/cache.py
@@ -4,12 +4,12 @@ import redis
 redis_url = os.getenv("REDIS_URL")
 
 try:
-    client = redis.from_url(
+    redis_client= redis.from_url(
         redis_url,
         decode_responses=True,
         socket_connect_timeout=5
     )
-    client.ping()
+    redis_client.ping()
     print("✅ Connected to Upstash Redis")
 except Exception as e:
     print("❌ Redis connection error:", e)
