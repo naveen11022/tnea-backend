8d4f9670cf68312a07da7c5cba654b466d10d2ec
Naveen Prasath S <naviprasaad50@gmail.com>
2026-02-28 08:26:39 +0530

    Update CORS middleware to allow additional origin

--- main.py
+++ main.py
@@ -25,7 +25,10 @@ app.include_router(district_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tnea.onrender.com"],
+    allow_origins=[
+        "https://tnea.onrender.com",
+        "https://tnea.onrender.com/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
